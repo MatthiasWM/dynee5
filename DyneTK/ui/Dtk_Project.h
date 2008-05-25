91920dc4ee20294fc3b83b48948b1e2e8e047d41
melcher.matthias <melcher.matthias@356354d2-2826-0410-b3cb-cff0e6766903>
2008-05-25 18:19:07 +0000

    Reorganizing: some renaming, won't compile in this revision!

---
+++ DyneTK/ui/Dtk_Project.h
@@ -0,0 +1,134 @@
+//
+// "$Id$"
+//
+// Dtk_Project header file for the Dyne Toolkit.
+//
+// Copyright 2007 by Matthias Melcher.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "dtk@matthiasm.com".
+//
+
+#ifndef DTK_PROJECT_H
+#define DTK_PROJECT_H
+
+extern "C" {
+#include "NewtType.h"
+}
+
+
+class Dtk_Document_List;
+class Dtk_Script_Writer;
+class Mgr_Project;
+class Dtk_Project_Manager;
+
+class Fl_Window;
+class Fl_Tabs;
+
+
+/*---------------------------------------------------------------------------*/
+/**
+ * Manage multiple documents that make up a project and generate and application.
+ */
+class Dtk_Project
+{
+public:
+	Dtk_Project();
+	~Dtk_Project();
+  
+  void setGuiManager(Dtk_Project_Manager *mgr) { guiManager = mgr; }
+
+    /** Check if the project has changed and needs to be saved.
+     * \todo Implement me!
+     */
+    int         isDirty() { return 0; }
+
+	void		setDefaults();
+	int			load();
+	int			loadMac();
+	int			loadWin();
+	int			save();
+
+	int			write(Dtk_Script_Writer &sw);
+
+    /** Save all dirty parts of the project.
+     */
+    int			saveAll();
+
+    /** Close and delete all dependent.
+     * \todo Implement me!
+     */
+	void		close() {}
+
+	int			buildPackage();
+	int			savePackage();
+
+	char		* getPackageName();
+	void		setFilename(const char *filename);
+	newtRef		makeFileRef(const char *filename);
+
+    /// the pathname of the file with backslash separators and a trailing backslash
+    char        * pathname();
+
+  /** Return the short name of the project.
+   */
+  const char  * name() { return name_; }
+  
+    const char  * filename() { return filename_; }
+    Dtk_Document_List   * documentList() { return documentList_; }
+
+protected:
+	void		pushDir();
+	void		popDir();
+
+private:
+
+	char		* packagename_;
+	char		* shortname_;
+	char		* filename_;
+	char		* name_;
+	char		* startdir_;
+	char		* pathname_;
+	newtRef		package_;
+
+  /// Keep a list of all documents in the project.
+  Dtk_Document_List   * documentList_;
+  
+  Dtk_Project_Manager * guiManager;
+};
+
+class Dtk_Project_Manager
+{
+public:
+  Dtk_Project_Manager(Dtk_Project*);
+  ~Dtk_Project_Manager();
+  void projectCreated();
+  void projectRemoved();
+  void projectRenamed();
+  
+private:
+  Dtk_Project *project;
+  Fl_Window *window;
+  Fl_Tabs *browserTabs;
+};
+    
+
+#endif
+
+//
+// End of "$Id$".
+//
