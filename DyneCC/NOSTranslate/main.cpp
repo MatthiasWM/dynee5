ebaf78ce57c965b003cf6d26ebdc2b1684813761
Matthias Melcher <git@matthiasm.com>
2017-01-13 00:39:43 +0100

    Start binary translator from scratch

---
+++ DyneCC/NOSTranslate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NOSTranslate
+//
+//  Created by Matthias Melcher on 1/13/17.
+//  Copyright © 2017 elektriktrick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
